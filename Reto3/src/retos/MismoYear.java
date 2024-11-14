66204157d34ae5fe7a08f6e6faab0820cd814e99
Tron89 <claudiuacricota@gmail.com>
2024-11-14 09:58:31 +0100

    fix bug

--- Reto3/src/retos/MismoYear.java
+++ Reto3/src/retos/MismoYear.java
@@ -18,7 +18,7 @@ public class MismoYear {
 		
 		String min = "";
 		String firstAlf = "";		
-		String str;
+		String str = "";
 		
 		for (int i = 0; i < num; i++) {
 			System.out.println("Dime un nombre");
@@ -30,23 +30,24 @@ public class MismoYear {
 				min = (str.length() <= min.length()) ? (str) : (min);
 				firstAlf = (firstAlf.compareTo(str)>0) ? (firstAlf) : (str);
 			}
+		
 			
-			System.out.println("Nombre de tamaño mas pequeño: "+min);
-			System.out.println("El primero alfabeticamente es: "+firstAlf);
+		}
+		
+		System.out.println("Nombre de tamaño mas pequeño: "+min);
+		System.out.println("El primero alfabeticamente es: "+firstAlf);
 
-			System.out.println("Las vocales son: ");
-			boolean first = true;
-			for (int j = 0; j < str.length(); j++) {
-				char chara = str.charAt(i);
-				if(chara=='a' || chara=='e' || chara=='i' || chara=='o' || chara=='u') {
-					if(first) {
-						System.out.print(chara);
-					} else {
-						System.out.println("-"+chara);
-					}
+		System.out.println("Las vocales son: ");
+		boolean first = true;
+		for (int j = 0; j < str.length(); j++) {
+			char chara = str.charAt(j);
+			if(chara=='a' || chara=='e' || chara=='i' || chara=='o' || chara=='u') {
+				if(first) {
+					System.out.print(chara);
+				} else {
+					System.out.println("-"+chara);
 				}
 			}
-			
 		}
 		
 	}
