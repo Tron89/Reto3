4ee3541ea8d0b14a85fa4586a5d0f814652b679f
bianca2912 <biacasimina01@gmail.com>
2024-11-14 09:02:14 +0100

    Hice lo del main

--- Reto3/src/retos/Reto3.java
+++ Reto3/src/retos/Reto3.java
@@ -1,10 +1,27 @@
 package retos;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import util.Funciones;
+
 public class Reto3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc= new Scanner (System.in);
+		LocalDate fecha1;
+		LocalDate fecha2;
+		
+		while (true) {
+			fecha1 = Funciones.dimeFechaStr("Introduce la primera fecha (día/mes/año): ", sc); 
+			fecha2 = Funciones.dimeFechaStr("Introduce la segunda fecha (día/mes/año):", sc);
+			if (fecha1.isBefore(fecha2)) {
+				break;
+			} System.out.println("Incorrecto, intentálo de nuevo.");
+				
+		}
 	}
 
 }
